02641a06746104f968e8dad9f3664a3642b5fd2e
bpull <m175148@usna.edu>
2016-04-25 10:25:44 -0400

    335proj3 work

--- 335proj3/plan.py
+++ 335proj3/plan.py
@@ -6,6 +6,20 @@
 
 import sys
 
+class grid:
+
+    def __init__ (self, obstacle, asset, target):
+        self.obstacle = obstacle
+        self.asset = asset
+        self.target = target
+        self.tracked = 0
+
+	# def __str__(self):
+	# 	return str(self.job_id) + " " + str(self.arrival) + " " + \
+	# 	str(self.service) + " " + str(self.priority)
+
+
+
 def main():
     if len(sys.argv) != 3:
         print ("USAGE: python3 plan.py <mapfile> <movesfile>")
@@ -14,15 +28,24 @@ def main():
     #create graph (adjacency list for double array)
     mapfile = open(str(sys.argv[1]), 'r')
     dimensions = mapfile.readline().split()
+    matrix = {}
+
+    numObstacles = mapfile.readline()
+    assetList = []
+
+    for i in range(0,   numObstacles):
+
+
 
     for x in range (0, int(dimensions[0])):
         for y in range (0, int(dimensions[1])):
 
+            name = str(x)+str(y)
             #check if x on border
             if x == 0:
-                
+                martix[name] = grid()
             if x == int(dimensions[0]) - 1:
-
+                print ("yes")
 
 
 if __name__ == '__main__':
